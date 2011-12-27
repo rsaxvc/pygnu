f69b43b420599e067e2a0b86dcba8fb11dc76f5f
Richard Allen <rsaxvc@gmail.com>
2011-12-26 22:02:50 -0600

    added --help

--- ls.py
+++ ls.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 import os,sys
 
-if( "--version" in sys.argv ):
+if("--help" in sys.argv ):
+	print "--help     display this help and exit"
+	print "--version  output version information and exit"
+
+elif( "--version" in sys.argv ):
 	import version
 	print "%s%s" % (sys.argv[0],version.package)
 	print version.copyright
