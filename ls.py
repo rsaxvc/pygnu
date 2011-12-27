4f87c62f881fb986a2df4c64afc3e75c11af01f3
Richard Allen <rsaxvc@gmail.com>
2011-12-26 22:00:12 -0600

    add --version support

--- ls.py
+++ ls.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 import os,sys
 
-paths = []
-if( len( sys.argv ) > 1 ):
-	paths.extend( sys.argv[ 1: ] )
+if( "--version" in sys.argv ):
+	import version
+	print "%s%s" % (sys.argv[0],version.package)
+	print version.copyright
 else:
-	paths.append( os.getcwd() )
-
-for path in paths:
-	if( len( paths ) > 1 ):
-		print "%s:" % path
-	for fname in os.listdir(path):
-		if fname[0] != '.':
-			print fname
-	if( len( paths ) > 1 ):
-		print 
+	paths = []
+	if( len( sys.argv ) > 1 ):
+		paths.extend( sys.argv[ 1: ] )
+	else:
+		paths.append( os.getcwd() )
+	for path in paths:
+		if( len( paths ) > 1 ):
+			print "%s:" % path
+		for fname in os.listdir(path):
+			if fname[0] != '.':
+				print fname
+		if( len( paths ) > 1 ):
+			print 
---
+++ version.py
@@ -0,0 +1,9 @@
+version = "0.1"
+package = "(pyGNU) %s" % version
+copyright = "Copyright (C) 2011 RSAXVC Development\n\
+License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>.\n\
+This is free software: you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.\n\
+\n\
+Written by Richard S. Allen."
+
