b827ab5a8e487273c07e1542641e7544fb971f5f
Richard Allen <rsaxvc@gmail.com>
2012-01-02 03:35:07 -0600

    update license note

--- version.py
+++ version.py
@@ -3,6 +3,9 @@ package = "(pyGNU) %s" % version
 copyright = "Copyright (C) 2011 RSAXVC Development\n\
 License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>.\n\
 This is free software: you are free to change and redistribute it.\n\
+\n\
+If you would like a different license, feel free to ask\n\
+\n\
 There is NO WARRANTY, to the extent permitted by law.\n\
 \n\
 Written by Richard S. Allen."
