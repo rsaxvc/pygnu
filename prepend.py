7f40f9468477acc98bad02451934c0fca26d9a55
Richard Allen <rsaxvc@gmail.com>
2013-09-06 22:10:03 -0500

    Update prepend.py to read iteratively and added better usage notes

--- prepend.py
+++ prepend.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 import fileinput,sys
 
-if( len( sys.argv ) != 2 ):
-	print "Usage: "+sys.argv[0]+" <string to prepend> "
-	sys.exit(1)
+if("--help" in sys.argv or len( sys.argv ) < 2 ):
+    print "Usage: %s [String] [optional file]"%sys.argv[0]
+    print "--help     display this help and exit"
+    print "--version  output version information and exit"
+elif( "--version" in sys.argv ):
+    import version
+    print "%s %s" % (sys.argv[0],version.package)
+    print version.copyright
+else:
+	prepend = str(sys.argv[1])
 
-prepend = str(sys.argv[1])
+	if( len( sys.argv ) == 2 ):
+		fd = sys.stdin
+	else:
+		fd = open( sys.argv[2] )
 
-for line in sys.stdin.readlines():
-	sys.stdout.write( prepend + line )
+	line_number = 0
+	for line in iter(fd.readline, ''):
+		line_number += 1
+		sys.stdout.write( prepend + line )
